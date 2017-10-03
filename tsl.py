4f0328dee8b5050cd2886d4bf7570363b33549d4
TheC <i@thec.me>
2017-10-03 14:02:50 +0800

    m16自动连点

--- tsl.py
+++ tsl.py
@@ -25,6 +25,27 @@ def move_mouse():
 			controller.move(0, GUN_PRESETS[GUN_TYPE]['value'])
 		time.sleep(cooldown)
 
+t = threading.Thread(target=move_mouse)
+t.setDaemon(True)
+t.start()
+
+# --
+# auto send click for M16A1
+def click_mouse():
+	global MOUSE_LEFT_DOWN, GUN_PRESETS, GUN_TYPE
+	cooldown = 0.2
+	while True:
+		if MOUSE_LEFT_DOWN and GUN_PRESETS[GUN_TYPE]['value'] == 'm16':
+			controller.click(mouse.Button.left)
+			cooldown = Math.uniform(0, 1)
+		time.sleep(cooldown)
+
+t2 = threading.Thread(target=click_mouse)
+t2.setDaemon(True)
+t2.start()
+
+# --
+
 def on_click(x, y, button, pressed):
 	global MOUSE_LEFT_DOWN, MOUSE_RIGHT_DOWN
 	# mouse right state
@@ -41,10 +62,6 @@ def on_scroll(x, y, dx, dy):
 	elif dy > 0 and GUN_TYPE < len(GUN_PRESETS) - 1:
 		GUN_TYPE += 1
 	print 'Weapon mode switched to %s' % GUN_PRESETS[GUN_TYPE]['name']
-
-t = threading.Thread(target=move_mouse)
-t.setDaemon(True)
-t.start()
 
 # Collect events until released
 with mouse.Listener(on_click=on_click, on_scroll=on_scroll) as listener:
