0e9972b9090fdc20721211f1646320eb0d41218b
TheC <i@thec.me>
2017-10-06 12:59:51 +0800

    改为键盘控制

--- tsl.py
+++ tsl.py
@@ -1,17 +1,19 @@
 import time, threading, random
-from pynput import mouse
-
-TEST_MODE = False
+from pynput import mouse, keyboard
 
 GUN_TYPE = 0
 GUN_PRESETS = [
 	{ 'name': 'M4', 'delta': 15, 'cd': 0.086 },
 	{ 'name': 'M16', 'delta': 11, 'cd': 0.075 },
-	{ 'name': 'Scarl', 'delta': 15, 'cd': 0.096 },
+	{ 'name': 'Scarl', 'delta': 19, 'cd': 0.096 },
+	{ 'name': 'Mini14', 'delta': 11.2, 'cd': 0.1 },
 	{ 'name': 'AKM', 'delta': 15, 'cd': 0.1 },
 	{ 'name': 'UMP9', 'delta': 15, 'cd': 0.092 },
 	{ 'name': 'Uzi', 'delta': 15, 'cd': 0.048 }
 ]
+
+ON = False
+TEST_MODE = False
 
 MOUSE_LEFT_DOWN = False
 MOUSE_RIGHT_DOWN = False
@@ -20,6 +22,47 @@ CLICK_SENT_BY_SCRIPT = 0
 MOUSE_Y_DELTA = 15
 
 controller = mouse.Controller()
+
+def init_keyboard():
+	def on_press(key):
+		global ON, TEST_MODE, MOUSE_Y_DELTA, GUN_TYPE, GUN_PRESETS
+		# turnOn/Off
+		if key == keyboard.Key.f7:
+			ON = not ON
+			print 'Program %s' % ('ON' if ON else 'OFF')
+		# do not handle other input when off
+		if not ON:
+			return True
+		# switch test mode
+		if key == keyboard.Key.f8:
+			TEST_MODE = not TEST_MODE
+			if TEST_MODE:
+				print 'Test mode %s' % ('ON' if TEST_MODE else 'OFF')
+		# page up
+		elif key == keyboard.Key.page_up:
+			if TEST_MODE:
+				MOUSE_Y_DELTA += 1
+				print 'Current mouse delta: %s' % MOUSE_Y_DELTA
+			elif GUN_TYPE < len(GUN_PRESETS) - 1:
+				GUN_TYPE += 1
+				print 'Weapon mode switched to %s' % GUN_PRESETS[GUN_TYPE]['name']
+		# page down
+		elif key == keyboard.Key.page_down:
+			if TEST_MODE:
+				MOUSE_Y_DELTA -= 1
+				print 'Current mouse delta: %s' % MOUSE_Y_DELTA
+			elif GUN_TYPE > 0 :
+				GUN_TYPE -= 1
+				print 'Weapon mode switched to %s' % GUN_PRESETS[GUN_TYPE]['name']
+
+	with keyboard.Listener(on_press=on_press) as listener:
+	    listener.join()
+
+k = threading.Thread(target=init_keyboard)
+k.setDaemon(True)
+k.start()
+
+# --
 
 def move_mouse():
 	global TEST_MODE, MOUSE_Y_DELTA, MOUSE_LEFT_DOWN, MOUSE_RIGHT_DOWN, GUN_PRESETS, GUN_TYPE
@@ -62,24 +105,6 @@ def on_click(x, y, button, pressed):
 		# change mouse right state
 		MOUSE_RIGHT_DOWN = not MOUSE_RIGHT_DOWN
 
-def on_scroll(x, y, dx, dy):
-	global TEST_MODE, MOUSE_Y_DELTA, GUN_TYPE, GUN_PRESETS
-	if not TEST_MODE:
-		# normal mode: scroll to switch weapon presets
-		if dy < 0 and GUN_TYPE > 0:
-			GUN_TYPE -= 1
-		elif dy > 0 and GUN_TYPE < len(GUN_PRESETS) - 1:
-			GUN_TYPE += 1
-		print 'Weapon mode switched to %s' % GUN_PRESETS[GUN_TYPE]['name']
-	else:
-		# test mode: scroll to incr/decr y delta
-		if dy < 0 and MOUSE_Y_DELTA > 0:
-			MOUSE_Y_DELTA -= 1
-		elif dy > 0:
-			MOUSE_Y_DELTA += 1
-		print 'Current mouse delta: %s' % MOUSE_Y_DELTA
-
-
 # listen
-with mouse.Listener(on_click=on_click, on_scroll=on_scroll) as listener:
+with mouse.Listener(on_click=on_click) as listener:
     listener.join()
